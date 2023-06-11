1c93756cdb50706c9a6c81ec88b998595c72ae47
Arghyadip-Biswas <135996888+Arghyadip-Biswas@users.noreply.github.com>
2023-06-11 12:18:02 +0530

    Update main.sh
    add trackers to aria2

--- main.sh
+++ main.sh
@@ -45,7 +45,8 @@ do
         sudo rm -rf $PWD/download
         break
     else
-        aria2c --max-connection-per-server=16 '$linkk'
+        TRACKERS=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_udp.txt | awk '$1' | tr '\n' ',')
+        aria2c --allow-overwrite=true --bt-enable-lpd=true --bt-max-peers=0 --bt-tracker="[$TRACKERS]" --check-certificate=false --follow-torrent=mem --max-connection-per-server=16 --max-overall-upload-limit=1K --peer-agent=qBittorrent/4.3.6 --peer-id-prefix=-qB4360- --seed-time=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --user-agent=qBittorrent/4.3.6 "$linkk"
         sudo cp -rf $dldir/* $rclonedir
         sudo rm -rf mountpoint/*
         sudo rm -rf download/*
