33cefa7e0d814356778962afc7f0fe3b7466ffdc
Arghyadip-Biswas <arghyadip.biswas123@gmail.com>
2023-11-21 21:57:44 +0530

    Rewrite and add telegram support

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,2 +1,3 @@
 sudo apt update
-sudo apt install --no-install-recommends -y rclone aria2 fuse
+sudo apt install --no-install-recommends -y rclone aria2 fuse python3 python3-pip
+sudo pip3 install -r requirements.txt
--- main.sh
+++ main.sh
@@ -1,54 +1,93 @@
-homedir=$PWD
-PID=$(pgrep -f rclone)
-if [ -z "$var" ];
-then
-echo
-else
-    sudo kill $PID
-fi
-if [ -d $PWD/mountpoint ];
-then 
-    pwd
-    sudo rm -rf $homedir/mountpoint
-    sudo rm -rf $homedir/download
-fi
-mkdir mountpoint
-mkdir download
-sudo chmod 777 *
-dldir=$PWD/download/
-rclonedir=$PWD/mountpoint/
-if [ -e $PWD/rclone.conf ];
-then 
-    echo "Rclone config file found !"
-else
-    echo "Enter rclone config link"
-    read rclone_link
-    wget $rclone_link
-fi
-echo "Enter where u want to upload: "
-echo "\n===================\n"
-sudo rclone --config=$PWD/rclone.conf listremotes
-echo "\n===================\n"
-read remote
-rclone mount --daemon --config=$PWD/rclone.conf $remote $rclonedir
-cd $dldir
-while true
-do
-    echo "Enter link (Enter q to exit): "
+#!/bin/bash
+
+function initt(){
+    if [[ mount | grep -q "$PWD/ul" ]]; then
+        echo -e "Rclone is mounted! Unmounting!"
+        kill $(pgrep -f rclone)
+        fusermount -u "$PWD/ul/"
+    if [[ -d $PWD/dl ]]; then
+        rm -rf dl ul
+    fi
+}
+
+function choicee(){
+    while true; do
+        echo -e "Where you want to upload ?\n    1.Cloud (Gdrive, OneDrive etc)\n    2.Telegram (Beta)\n    3.Exit"
+        read choice
+        if [[ $choice == 1 || $choice == 2 || $choice == 3 ]]; then
+            break
+        else
+            echo -e "\nInvalid input. Please enter a number between 1 and 2."
+        fi
+    done
+}
+
+function rclone_mount(){
+    if [ -e $PWD/rclone.conf ]; then 
+        echo -e "Rclone config file found !\n"
+    else
+        echo "Enter rclone config link : "
+        read rclone_link
+        wget $rclone_link
+    fi
+    echo -e "Enter where you want to upload: \n==================="
+    rclone --config=$PWD/rclone.conf listremotes
+    echo -e "===================\n"
+    read remote
+    rclone mount --daemon --config=$PWD/rclone.conf $remote "$PWD/ul"
+}
+
+function dl_start(){
+    echo -e "Enter link (Enter q to exit) : "
     read linkk
-    if [ $linkk == q ];
-    then
-        PID=$(pgrep -f rclone)
-        sudo kill $PID
-        cd ..
-        sudo rm -rf $PWD/mountpoint
-        sudo rm -rf $PWD/download
-        break
+    echo $linkk
+    if [[ "$linkk" != "https://"* ]]; then
+        clear
+        echo -e "Not a link! Exiting! Bye"
+        exit
+    elif [[ "$linkk" == "q" ]]; then
+        initt
+        clear
+        echo "Thanks for using my! Bye buddy!"
+        exit
+    elif [[ "$linkk" == "https://drive.google.com/"* ]]; then 
+        echo -e "\nGdrive link detected! Downloading..."
+        gdown -O "$PWD/dl/" "$linkk"
     else
         TRACKERS=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_udp.txt | awk '$1' | tr '\n' ',')
-        aria2c --allow-overwrite=true --bt-enable-lpd=true --bt-max-peers=0 --bt-tracker="[$TRACKERS]" --check-certificate=false --follow-torrent=mem --max-connection-per-server=16 --max-overall-upload-limit=1K --peer-agent=qBittorrent/4.3.6 --peer-id-prefix=-qB4360- --seed-time=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --user-agent=qBittorrent/4.3.6 "$linkk"
-        sudo cp -rf $dldir/* $rclonedir
-        sudo rm -rf mountpoint/*
-        sudo rm -rf download/*
-    fi  
-done
+        aria2c --allow-overwrite=true --bt-enable-lpd=true --bt-max-peers=0 --bt-tracker="[$TRACKERS]" --check-certificate=false --follow-torrent=mem --max-connection-per-server=16 --max-overall-upload-limit=1K --peer-agent=qBittorrent/4.3.6 --peer-id-prefix=-qB4360- --seed-time=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --user-agent=qBittorrent/4.3.6 -d "$PWD/dl/" "$linkk"  
+    fi
+}
+
+function rclone_up(){
+    cp "$PWD/dl/$custom_folder"* $PWD/ul/
+    rm -rf $PWD/dl/*
+}
+
+#################### End of Functions #############
+
+clear
+custom_folder="" # custom path of cloud
+mkdir dl ul
+chmod 777 dl ul
+choicee
+if [[ $choice == 1 ]]; then
+    if [[ mount | grep -q "$PWD/ul" ]]; then
+        echo -e "Rclone is already mounted!"
+    else
+        rclone_mount
+    fi
+    while true; do
+        dl_start
+        rclone_up
+    done
+elif [[ $choice == 2 ]]; then
+    echo "Work in progress! Currently in beta state!"
+    dl_start
+    python3 up.py $PWD/dl/*
+elif [[ $choice == 1 ]]; then
+    initt
+    exit
+else
+    echo "Bruh"
+fi
