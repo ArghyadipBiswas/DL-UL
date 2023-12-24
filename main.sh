c2ee7357063ac9a65d5076f482fa6d05942b8697
ArghyadipBiswas <rootg998@gmail.com>
2023-12-25 00:20:39 +0530

    Gdrive links now more stable

--- main.sh
+++ main.sh
@@ -21,11 +21,20 @@ function dl_start(){
         echo -e "Not a link! Exiting! Bye"
         exit
     elif [[ "$linkk" == "https://drive.google.com/"* ]]; then 
-        echo -e "\nGdrive link detected! Downloading..."
-        gdown -O "$PWD/dl/" "$linkk"
+        if echo "$linkk" | grep -q "folder"; then
+            echo -e "\n\nGdrive folder detected! Downloading!"
+            gdown --folder -O "$PWD/dl/" "$linkk"
+        elif echo "$linkk" | grep -q "file"; then
+            file_id=$(echo $linkk | sed -n 's/.*\/d\/\([^\/]*\)\/.*/\1/p')
+            echo -e "\n\nGdrive single file detected! Downloading!"
+            linkk="https://drive.google.com/uc?id=$file_id"
+            gdown -O "$PWD/dl/" "$linkk"
+        else
+            gdown -O "$PWD/dl/" "$linkk"
+        fi
     else
         TRACKERS=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_udp.txt | awk '$1' | tr '\n' ',')
-        aria2c --allow-overwrite=true --bt-enable-lpd=true --bt-max-peers=0 --bt-tracker="[$TRACKERS]" --check-certificate=false --follow-torrent=mem --max-connection-per-server=16 --max-overall-upload-limit=1K --peer-agent=qBittorrent/4.3.6 --peer-id-prefix=-qB4360- --seed-time=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --user-agent=qBittorrent/4.3.6 -d "$PWD/dl/" "$linkk"  
+        aria2c --allow-overwrite=true --bt-enable-lpd=true --bt-max-peers=0 --bt-tracker="[$TRACKERS]" --check-certificate=false --follow-torrent=mem --max-connection-per-server=16 --max-overall-upload-limit=1K -d "$PWD/dl/" "$linkk"  
     fi
 }
 
