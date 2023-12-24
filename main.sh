73625dd7179869238d126d45e37238045ecb01cc
ArghyadipBiswas <rootg998@gmail.com>
2023-12-24 18:15:08 +0530

    Big change & improve telegram upload

--- main.sh
+++ main.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-function initt(){
-    if [[ $(mount | grep -q "$PWD/ul") ]]; then
-        echo -e "Rclone is mounted! Unmounting!"
-        kill $(pgrep -f rclone)
-        fusermount -u "$PWD/ul/"
-    fi
-    if [[ -d $PWD/dl && -d $PWD/ul ]]; then
-        rm -rf dl ul
-    fi
-}
-
 function choicee(){
     while true; do
         echo -e "Where you want to upload ?\n    1.Cloud (Gdrive, OneDrive etc)\n    2.Telegram\n    3.Exit"
@@ -18,36 +7,16 @@ function choicee(){
         if [[ $choice == 1 || $choice == 2 || $choice == 3 ]]; then
             break
         else
-            echo -e "\nInvalid input. Please enter a number between 1 and 2."
+            echo -e "\nInvalid input. Please enter a number between 1-3"
         fi
     done
 }
 
-function rclone_mount(){
-    if [ -e $PWD/rclone.conf ]; then 
-        echo -e "Rclone config file found !\n"
-    else
-        echo "Enter rclone config link : "
-        read rclone_link
-        wget $rclone_link
-    fi
-    echo -e "Enter where you want to upload: \n==================="
-    rclone --config=$PWD/rclone.conf listremotes
-    echo -e "===================\n"
-    read remote
-    rclone mount --daemon --config=$PWD/rclone.conf $remote "$PWD/ul"
-}
-
 function dl_start(){
-    echo -e "Enter link (Enter q to exit) : "
+    clear
+    echo -e "Enter link (Enter any key to exit): "
     read linkk
-    echo $linkk
-    if [[ "$linkk" == "q" ]]; then
-        initt
-        clear
-        echo "Thanks for using my! Bye buddy!"
-        exit
-    elif [[ "$linkk" != "https://"* ]]; then
+    if [[ "$linkk" != "https://"* ]]; then
         clear
         echo -e "Not a link! Exiting! Bye"
         exit
@@ -60,54 +29,74 @@ function dl_start(){
     fi
 }
 
-function rclone_up(){
-    cp "$PWD/dl/"* "$PWD/ul/$custom_folder"
-    rm -rf $PWD/dl/*
-    echo "Upload Done!"
+function initt(){
+    if [[ -d $PWD/dl ]]; then
+        rm -rf dl ul
+    fi
 }
 
-function uploadd(){
-    find $PWD/dl -type f -exec python3 up.py {} \;
+function rclone_setup(){
+    if [ -e $PWD/rclone.conf ]; then 
+        echo -e "Rclone config file found !\n"
+    else
+        echo "Enter rclone config link : "
+        read rclone_link
+        aria2c $rclone_link
+    fi
+    echo -e "Enter where you want to upload: \n==================="
+    rclone --config=$PWD/rclone.conf listremotes
+    echo -e "===================\n"
+    read remote
+}
+
+function rclone_up(){
+    # clear
+    rclone --config=$PWD/rclone.conf move --transfers=10 --buffer-size 256M -P $PWD/dl/ $1\/$custom_folder
+    rm -rf $PWD/dl/*
+    echo "Upload Done!"
 }
 
 function splitt(){
     for filename in "$(find $PWD/dl -type f)"; do
         if [[ $(wc -c < "$filename") -ge 2000000000 ]]; then
             echo "File is greater than 2GiB! Splitting"
-            find $PWD/dl -type f -size +2G -exec split -b 2000m {} {}_part \; -exec rm {} \; -exec python3 up.py {} \;
+            find $PWD/dl -type f -size +2G -exec split -b 2000m {} {}_part \; -exec rm {} \;
         fi
     done
 }
 
-#################### End of Functions #############
+function tg_upload(){
+    find $PWD/dl -type f -exec python3 up.py {} \;
+}
+
+#====================================================================================
 
 clear
-custom_folder="" # custom path of cloud
-if [[ -d $PWD/ul && -d $PWD/dl ]]; then
+custom_folder="testing/" # custom path of cloud
+
+if [[ -d $PWD/dl ]]; then
     echo "No need to create directory again!"
 else
-    mkdir dl ul
-    chmod 777 dl ul
+    mkdir dl
+    chmod 777 dl
 fi
 choicee
 if [[ $choice == 1 ]]; then
-    if [[ $(mount | grep -q "$PWD/ul") ]]; then
-        echo -e "Rclone is already mounted!"
-    else
-        rclone_mount
-    fi
+    rclone_setup
     while true; do
         dl_start
-        rclone_up
+        rclone_up $remote
     done
 elif [[ $choice == 2 ]]; then
-    dl_start
-    splitt
-    echo "Starting upload!"
-    sleep 2
-    uploadd
-    rm -rf $PWD/dl/*
-    echo "Done! Cleaning"
+    while true; do
+        dl_start
+        splitt
+        echo "Starting upload!"
+        sleep 1
+        tg_upload
+        rm -rf $PWD/dl/*
+        echo "Done! Cleaning"
+    done
 elif [[ $choice == 3 ]]; then
     initt
     exit
--- up.py
+++ up.py
@@ -3,23 +3,33 @@ import time
 import sys
 
 filename = sys.argv[1]
+
 def progress(current, total):
-    print(f"Uploaded: ({current/1000000})/({total/1000000}) MB")
+    percent = (current / total) * 100
+    green_hashes = '❄' * int(percent / 2)
+    spaces = ' ' * (50 - int(percent / 2))
+    
+    # ANSI escape code for green color
+    green_color_code = '\033[92m'
+    reset_color_code = '\033[0m'
+    
+    sys.stdout.write(f"\rProgress: [{int(percent)}%] [{green_color_code}{green_hashes}{reset_color_code}{spaces}] ({current/1000000:.2f}/{total/1000000:.2f} MB)")
+    sys.stdout.flush()
 
 bot = Client(
     "my project",
-    api_id=12345678,
+    api_id=,
     api_hash="",
     bot_token=""
 )
+
 print("Uploading...")
+
 with bot:
     bot.send_document(
-        chat_id=5042525177,
+        chat_id=5115463777,
         document=filename,
         progress=progress
     )
-    exit()
 
-bot.run()
-exit()
+print("\nUpload complete.")
